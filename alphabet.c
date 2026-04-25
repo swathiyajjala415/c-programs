f7a543adcef38adb2cab68ca544bf733f53204c4
swathiyajjala415 <swathiyajjala120@gmail.com>
2026-04-25 12:18:13 +0530

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+    char ch;
+    printf("enter the character \n");
+    scanf("%c",&ch);
+    int res=(ch>='a')&&(ch<='z');
+    printf("%c is a alphabet i.e is: %d",ch,res);
+return 0;
+}
